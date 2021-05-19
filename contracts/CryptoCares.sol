6f48e012b347c4638d0028dc38adbd533918d091
Aditya Dhir <31381639+adi44@users.noreply.github.com>
2021-05-19 11:43:28 +0530

    Resolved Bugs and Added a method to find service

--- contracts/CryptoCares.sol
+++ contracts/CryptoCares.sol
@@ -30,6 +30,7 @@ contract Cryptocares {
     
     event ServiceAdded(uint service_id, address service_provider);
     event ServiceDisabled(uint service_id);
+    event ServiceEnabled(uint service_id);
     event ServiceProviderAdded(uint id,string uri);
     
     function Add_Service_Provider(
@@ -59,7 +60,7 @@ contract Cryptocares {
                     
                     address payable _NGOaddress = payable(address(0x68A99f89E475a078645f4BAC491360aFe255Dff1)); //address of COVID CryptoRelief India Fund
                     
-                    Services_list[_service_id] = Services(_service_id,_service_provider, _NGOaddress, _minimum_donation_amount,_duration, _amountOfServices,false);
+                    Services_list[_service_id] = Services(_service_id,_service_provider, _NGOaddress, _minimum_donation_amount,_duration, _amountOfServices,true);
                     
                     emit ServiceAdded(_service_id,_service_provider);
                                                     
@@ -68,12 +69,14 @@ contract Cryptocares {
                     
                     }
                         
-    function Avail_Service(uint id) payable public
+    function Avail_Service(uint256 id) payable public
     {
-       Services memory service = Services_list[id];
+       Services storage service = Services_list[id];
        uint256 _amountOfServices = service.amountOfServices;
        require(_amountOfServices > 0, "All available services have been minted");
+       service.amountOfServices = service.amountOfServices - 1;
        address payable _toNGO = service.NGOaddress;
+       
        _toNGO.transfer(msg.value);
        
        
@@ -82,7 +85,7 @@ contract Cryptocares {
     
     function Disable_Service(uint256 id) public {
         
-        Services memory service = Services_list[id];
+        Services storage service = Services_list[id];
         
         require(msg.sender == service.service_provider,"You are not Service Provider");
         require(service.service_disable!=false,"Service is already disabled");
@@ -97,16 +100,21 @@ contract Cryptocares {
     
     function Enable_Service(uint256 id) public {
         
-        Services memory service = Services_list[id];
+        Services storage service = Services_list[id];
         
         require(msg.sender == service.service_provider,"You are not Service Provider");
         require(service.service_disable!=true,"Service is already enabled");
         
         service.service_disable=true;
         
-        emit ServiceDisabled(service.service_id);
+        emit ServiceEnabled(service.service_id);
         
     }
     
+    function Get_Service(uint256 id) public returns(Services memory){
+        return Services_list[id];
+    }
+    
 }
+
 
