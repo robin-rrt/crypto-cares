320e7db65d44a82a5ba58f547b96d66c77be72c7
John Koodarathil Varkey <varkey100john@gmail.com>
2021-05-31 03:14:05 +0400

    added table

--- frontend/src/Pages/CreateService.js
+++ frontend/src/Pages/CreateService.js
@@ -1,77 +1,279 @@
-import React,{getGlobal,useGlobal, useState} from 'reactn'
-import { Form, Button } from 'react-bootstrap'
-import './createService.css'
-import Web3 from 'web3'
-import ABI from '../contracts/newABI.json'
+import React, { getGlobal, useGlobal, useState } from "reactn";
+import { Form } from "react-bootstrap";
+import "./createService.css";
+import Web3 from "web3";
+import ABI from "../contracts/newABI.json";
+// import "./DonateForm.css";
+
+import {
+  Container,
+  CssBaseline,
+  Grid,
+  Paper,
+  Typography,
+  TextField,
+  MenuItem,
+  Button,
+  makeStyles,
+  MenuList,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: theme.spacing(10),
+    backgroundColor: "#FFF7FF",
+  },
+  adornment: {
+    paddingRight: "0px",
+    backgroundColor: "#EFF0F6",
+    borderRadius: "16px",
+  },
+  textfield: {
+    height: "64px",
+    borderRadius: "16px",
+    backgroundColor: "#EFF0F6",
+    "&:active": {
+      borderColor: " #14142B",
+    },
+  },
+  select: {
+    height: "64px",
+    borderRadius: "16px",
+
+    "&:active": {
+      borderColor: " #14142B",
+    },
+  },
+  donate: {
+    backgroundColor: "#14142B",
+    color: "white",
+    height: "64px",
+    borderRadius: "16px",
+  },
+}));
 
 const CreateService = () => {
-    const [account] = useGlobal('account')
-    const [web3]= useGlobal('web3')
-    const [minDonation, setMinDonation] = useState('minDonation')
-    const [duration, setDuration] = useState('duration')
-    const [amount, setAmount] = useState('amount')
-    const [description, setDescription] = useState('description')
-    const [uri, setUri] = useState('uri')
+  const classes = useStyles();
+  const [account] = useGlobal("account");
+  const [web3] = useGlobal("web3");
+  const [data, setData] = useState({
+    minDonation: "",
+    duration: "",
+    amount: "",
+    description: "",
+    donateAmount: "",
+    uri: "",
+    unit: "",
+  });
 
-    // const { abi } = require('../contracts/Cryptocares.json')
+  const handleInputChange = (event) => {
+    setData((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.value,
+    }));
+  };
 
-    // const [web3] = useGlobal('web3')
- 
+  var contract = new web3.eth.Contract(
+    ABI,
+    "0x0743c50FF0FBAb7EBcC0D2DE7F6884a72fF2c576"
+  );
 
-    var contract = new web3.eth.Contract(ABI, '0x0743c50FF0FBAb7EBcC0D2DE7F6884a72fF2c576');
+  const handleSubmit = async (event) => {
+    let {
+      minDonation,
+      duration,
+      amount,
+      description,
+      donateAmount,
+      uri,
+      unit,
+    } = data;
+    event.preventDefault();
+    console.log("minimum donation amount: " + minDonation);
+    console.log(ABI[5]);
+    console.log(contract.methods);
+    contract.methods
+      .Add_Services(minDonation, duration, amount, description, uri)
+      .send({ from: account })
+      .on("transactionHash", (hash) => {
+        console.log(hash);
+      })
+      .on("receipt", (receipt) => {
+        console.log(receipt);
+      });
+  };
 
-    const handleSubmit = async event => {
-        
-        event.preventDefault();
-        console.log("minimum donation amount: " + minDonation)
-        console.log(ABI[5])
-        console.log(contract.methods)
-        contract.methods.Add_Services(minDonation, duration, amount, description, uri).send({from: account})
-        .on('transactionHash', (hash) => {
-            console.log(hash)
-        })
-        .on('receipt', (receipt) => {
-            console.log(receipt)
-        })
-        
-    }
+  if (!account) {
     return (
-        <div className="create-service-container">   
-        
-            <div className="create-service-form">
-                {
-                    
-                    !!account
-                    ?
-                        
-                        <Form.Group>
-                            <Form onSubmit={handleSubmit}>
-                                <h2 className="service-title">Create Service</h2>
-                                
-                                <Form.Control className="service-input-field" placeholder="Minimum Donation Amount ($)" onChange={e => setMinDonation(e.target.value)} ></Form.Control>
-                                <br />
-                                <Form.Control className="service-input-field" placeholder="Duration of Service (days)" onChange={e => setDuration(e.target.value)}></Form.Control>
-                                <br />
-                                <Form.Control className="service-input-field" placeholder="Amount of Services" onChange={e => setAmount(e.target.value)}></Form.Control>
-                                <br />
-                                <Form.Control className="service-input-field" as="textarea" placeholder="Description of Service" rows={3} onChange={e => setDescription(e.target.value)}></Form.Control>
-                                <br />
-                                <Form.Control className="service-input-field" placeholder="Contact URI" onChange={e => setUri(e.target.value)}></Form.Control>
-                                <br />
-                                <Button type="submit">Create Service</Button>
-                            </Form>
-                        
-                         </Form.Group>
-                    :
-                    <h1>Account not Connected</h1>
-                } 
-                
-            </div>
+      <>
+        <CssBaseline />
+        <Container className={classes.root}>
+          <main>
+            <Paper>
+              <Grid container spacing={6} alignItems="center" justify="center">
+                <Grid item xs={12}>
+                  <center>
+                    <h1> Account Not Connected</h1>
+                  </center>
+                </Grid>
+              </Grid>
+              <br />
+            </Paper>
+          </main>
+        </Container>
+      </>
+    );
+  } else
+    return (
+      <>
+        <CssBaseline />
+        <Container maxWidth="md" className={classes.root}>
+          <main>
+            <Paper>
+              <Grid container spacing={6} alignItems="center" justify="center">
+                <Grid item xs={10}>
+                  <Typography variant="h1" component="h1">
+                    Create Services
+                  </Typography>
+                </Grid>
+                <Grid item xs={10}>
+                  <TextField
+                    variant="outlined"
+                    label="Minimum Donation Amount"
+                    name="minDonation"
+                    fullWidth
+                    value={data.minDonation}
+                    InputProps={{
+                      className: classes.textfield,
+                    }}
+                    className={classes.textfield}
+                    onChange={handleInputChange}
+                  />
+                </Grid>
+                <Grid item xs={10}>
+                  <TextField
+                    variant="outlined"
+                    label="Duration of Service"
+                    name="duration"
+                    fullWidth
+                    value={data.duration}
+                    InputProps={{
+                      className: classes.textfield,
+                    }}
+                    className={classes.textfield}
+                    onChange={handleInputChange}
+                  />
+                </Grid>
+                <Grid item xs={7}>
+                  <TextField
+                    variant="outlined"
+                    label="Amount of Service"
+                    type="number"
+                    name="amount"
+                    fullWidth
+                    value={data.amount}
+                    InputProps={{
+                      className: classes.textfield,
+                    }}
+                    className={classes.textfield}
+                    onChange={handleInputChange}
+                  />
+                </Grid>
+                <Grid item xs={3}>
+                  <TextField
+                    variant="outlined"
+                    name="unit"
+                    label="Unit"
+                    select
+                    fullWidth
+                    value={data.unit}
+                    InputProps={{
+                      className: classes.textfield,
+                    }}
+                    className={classes.textfield}
+                    onChange={handleInputChange}
+                  >
+                    <MenuItem value="Matic">Matic</MenuItem>
+                  </TextField>
+                </Grid>
+                <Grid item xs={10}>
+                  <TextField
+                    variant="outlined"
+                    name="description"
+                    label="Description"
+                    fullWidth
+                    value={data.description}
+                    InputProps={{
+                      className: classes.textfield,
+                    }}
+                    className={classes.textfield}
+                    onChange={handleInputChange}
+                  />
+                </Grid>
 
-          
-        </div>
-
-    )
-}
+                <Grid item xs={10}>
+                  <TextField
+                    variant="outlined"
+                    label="Contact URI"
+                    name="uri"
+                    fullWidth
+                    value={data.uri}
+                    InputProps={{
+                      className: classes.textfield,
+                    }}
+                    className={classes.textfield}
+                    onChange={handleInputChange}
+                  />
+                </Grid>
+                <Grid item xs={10}>
+                  <Button
+                    className={classes.donate}
+                    fullWidth
+                    onClick={handleSubmit}
+                  >
+                    Create Service
+                  </Button>
+                </Grid>
+              </Grid>
+              <br />
+            </Paper>
+          </main>
+        </Container>
+      </>
+    );
+};
 
 export default CreateService;
+
+/**
+
+   <div className="create-service-container">
+        <div className="create-service-form">
+          {!!account ? (
+            <Form.Group>
+              <Form onSubmit={handleSubmit}>
+                <h2 className="service-title">Create Service</h2>
+
+              
+                <br />
+               
+                <br />
+               
+                <br />
+                <br />
+                <Form.Control
+                  className="service-input-field"
+                  placeholder="Contact URI"
+                  onChange={(e) => setUri(e.target.value)}
+                ></Form.Control>
+                <br />
+                <Button type="submit">Create Service</Button>
+              </Form>
+            </Form.Group>
+          ) : (
+            <h1>Account not Connected</h1>
+          )}
+        </div>
+      </div>
+
+ */
--- frontend/src/Pages/Donate.js
+++ frontend/src/Pages/Donate.js
@@ -1,14 +1,104 @@
-import React, { useGlobal } from 'reactn'
-import DonateForm from '../components/DonateForm'
+import React, { useGlobal, useState, useRef, useEffect } from "reactn";
+import ABI from "../contracts/newABI.json";
+import AccountBalanceWalletIcon from "@material-ui/icons/AccountBalanceWallet";
+import { TableComponent } from "../components/TableComponent";
+import DonateTableCol from "../components/DonateTableCol";
+import {
+  Container,
+  CssBaseline,
+  Grid,
+  Paper,
+  makeStyles,
+} from "@material-ui/core";
 
-export default function Donate (){
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: theme.spacing(10),
+    backgroundColor: "#FFF7FF",
+  },
+  grow: {
+    flexGrow: 1,
+  },
+}));
 
-    const [account] = useGlobal('account')
-    
-        return (
-            <div>
-                {!!account?<DonateForm  />:<h1>Account Not Connected</h1>}
-            </div>
-        )
-    
+export default function Donate() {
+  const [account] = useGlobal("account");
+  const [web3] = useGlobal("web3");
+  const classes = useStyles();
+  const tableRef = useRef(null);
+  const [data, setData] = useState([
+    {
+      service_id: "test value",
+      Relief_fund_name: "test value",
+      service_provider: "test value",
+      minimum_donation_amount: "test value",
+      service_description: "test value",
+    },
+  ]);
+  useEffect(() => {
+    //TODO: set Data;
+  }, []);
+
+  const actions = [
+    //TODO: Find icon from  https://material-ui.com/components/material-icons/
+    (rowData) => ({
+      icon: () => <AccountBalanceWalletIcon />,
+      tooltip: "Donate",
+      onClick: (event, rowData) => {
+        console.log({ rowData });
+      },
+    }),
+  ];
+
+  if (!account) {
+    return (
+      <>
+        <CssBaseline />
+        <Container className={classes.root}>
+          <main>
+            <Paper>
+              <Grid container spacing={6} alignItems="center" justify="center">
+                <Grid item xs={12}>
+                  <center>
+                    <h1> Account Not Connected</h1>
+                  </center>
+                </Grid>
+              </Grid>
+              <br />
+            </Paper>
+          </main>
+        </Container>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <CssBaseline />
+        <Container className={classes.root}>
+          <main>
+            <Grid container alignItems="center" justify="center">
+              <Grid item xs={12}>
+                <TableComponent
+                  className={classes.table}
+                  tableRef={tableRef}
+                  actions={actions}
+                  columns={DonateTableCol}
+                  data={data}
+                  Title="Employee Management"
+                  options={{
+                    search: true,
+                    sorting: true,
+                  }}
+                  style={{
+                    flexGrow: 1,
+                  }}
+                />
+              </Grid>
+            </Grid>
+            <br />
+          </main>
+        </Container>
+      </>
+    );
+  }
 }
